69eff42a3786a42e6e64e13a7a6c7da1c15a48b1
AsgarovElchin <elcinaskerovki@gmail.com>
2025-03-07 22:46:48 +0400

    first commit

---
+++ src/test/java/elchinasgarov/plantly_backend/PlantlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package elchinasgarov.plantly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
